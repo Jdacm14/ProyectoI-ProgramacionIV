dd9df742d1f72322dec270c1f580e21fb7c3426e
Randy <randy040704@gmail.com>
2025-03-21 14:21:15 -0600

    Repo y otras cosas

--- src/main/java/com/example/proyectoiprogramacioniv/models/CitaModel.java
+++ src/main/java/com/example/proyectoiprogramacioniv/models/CitaModel.java
@@ -5,16 +5,16 @@ import jakarta.persistence.Entity;
 @Entity
 public class CitaModel {
     @Id
-    private int identificacion;
+    private String identificacion;
     private String medicoID;
     private String pacienteID;
-    private int horarioID;
+    private String horarioID;
     private String estado; //Pendiente, Completada, Cancelada
     private float precio;
 
     public CitaModel() {}
 
-    public CitaModel(int id, String medicoID, String pacienteID, int horarioID ) {
+    public CitaModel(String id, String medicoID, String pacienteID, String horarioID ) {
         this.identificacion = id;
         this.medicoID = medicoID;
         this.pacienteID = pacienteID;
@@ -24,26 +24,26 @@ public class CitaModel {
     }
 
     //      Getters
-    public int getIdentificacion() {return identificacion;}
+    public String getIdentificacion() {return identificacion;}
 
     public String getMedicoID() {return medicoID;}
 
     public String getPacienteID() {return pacienteID;}
 
-    public int getHorarioID() {return horarioID;}
+    public String getHorarioID() {return horarioID;}
 
     public String getEstado() {return estado;}
 
     public float getPrecio() {return precio;}
 
     //      setters
-    public void setIdentificacion(int identificacion) {this.identificacion = identificacion;}
+    public void setIdentificacion(String identificacion) {this.identificacion = identificacion;}
 
     public void setMedicoID(String medicoID) {this.medicoID = medicoID;}
 
     public void setPacienteID(String pacienteID) {this.pacienteID = pacienteID;}
 
-    public void setHorarioID(int horarioID) {this.horarioID = horarioID;}
+    public void setHorarioID(String horarioID) {this.horarioID = horarioID;}
 
     public void setEstado(String estado) {this.estado = estado;}
 
