6af900d3bbfcc2816935c8d5ec1067325c9372f8
JOSE CARBALLO MORALES <josedcarballom1404@gmail.com>
2025-03-23 12:34:09 -0600

    añadí atributos al medicos para llenarlos en su perfil y footer arreglado

--- src/main/java/com/example/proyectoiprogramacioniv/models/MedicoModel.java
+++ src/main/java/com/example/proyectoiprogramacioniv/models/MedicoModel.java
@@ -1,13 +1,7 @@
 package com.example.proyectoiprogramacioniv.models;
 
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-
-import java.time.Duration;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 public class MedicoModel {
@@ -18,20 +12,9 @@ public class MedicoModel {
     private String contrasenna;
     private boolean activo;
     private String especialidad;
-    private double costoConsulta;
     private String ubicacion;
-    private String presentacion;
 
-    private LocalTime horaInicio;
-    private LocalTime horaFin;
-    private Duration frecuencia;
-
-    @ElementCollection
-    private List<LocalTime> horariosDisponibles;
-
-    public MedicoModel() {
-        horariosDisponibles = new ArrayList<>();
-    }
+    public MedicoModel() {}
     public MedicoModel(String identificacion, String nombre, String contrasenna) {
         this.identificacion = identificacion;
         this.nombre = nombre;
@@ -39,114 +22,22 @@ public class MedicoModel {
         this.activo = false;
         this.especialidad = "";
         this.ubicacion = "";
-        this.presentacion = "";
-        this.horaInicio = null;
-        this.horaFin = null;
-        this.frecuencia = null;
-        horariosDisponibles = new ArrayList<>();
     }
 
-    public void generarHorariosDisponibles() {
-        LocalTime temp = this.horaInicio;
-        while (temp.isBefore(this.horaFin)) {
-            horariosDisponibles.add(temp);
-            temp = temp.plus(frecuencia);
-        }
-    }
+    // getters
+    public String getIdentificacion() {return identificacion;}
+    public String getNombre() {return nombre;}
+    public String getContrasenna() {return contrasenna;}
+    public boolean isActivo() {return activo;}
+    public String getEspecialidad() {return especialidad;}
+    public String getUbicacion() {return ubicacion;}
 
-    public String getIdentificacion() {
-        return identificacion;
-    }
+    // setters
+    public void setNombre(String nombre) {this.nombre = nombre;}
+    public void setIdentificacion(String identificacion) {this.identificacion = identificacion;}
+    public void setContrasenna(String contrasenna) {this.contrasenna = contrasenna;}
+    public void setActivo(boolean activo) {this.activo = activo;}
+    public void setEspecialidad(String especialidad) {this.especialidad = especialidad;}
+    public void setUbicacion(String ubicacion) {this.ubicacion = ubicacion;}
 
-    public void setIdentificacion(String identificacion) {
-        this.identificacion = identificacion;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getContrasenna() {
-        return contrasenna;
-    }
-
-    public void setContrasenna(String contrasenna) {
-        this.contrasenna = contrasenna;
-    }
-
-    public boolean isActivo() {
-        return activo;
-    }
-
-    public void setActivo(boolean activo) {
-        this.activo = activo;
-    }
-
-    public String getEspecialidad() {
-        return especialidad;
-    }
-
-    public void setEspecialidad(String especialidad) {
-        this.especialidad = especialidad;
-    }
-
-    public double getCostoConsulta() {
-        return costoConsulta;
-    }
-
-    public void setCostoConsulta(double costoConsulta) {
-        this.costoConsulta = costoConsulta;
-    }
-
-    public String getUbicacion() {
-        return ubicacion;
-    }
-
-    public void setUbicacion(String ubicacion) {
-        this.ubicacion = ubicacion;
-    }
-
-    public String getPresentacion() {
-        return presentacion;
-    }
-
-    public void setPresentacion(String presentacion) {
-        this.presentacion = presentacion;
-    }
-
-    public LocalTime getHoraInicio() {
-        return horaInicio;
-    }
-
-    public void setHoraInicio(LocalTime horaInicio) {
-        this.horaInicio = horaInicio;
-    }
-
-    public LocalTime getHoraFin() {
-        return horaFin;
-    }
-
-    public void setHoraFin(LocalTime horaFin) {
-        this.horaFin = horaFin;
-    }
-
-    public Duration getFrecuencia() {
-        return frecuencia;
-    }
-
-    public void setFrecuencia(Duration frecuencia) {
-        this.frecuencia = frecuencia;
-    }
-
-    public List<LocalTime> getHorariosDisponibles() {
-        return horariosDisponibles;
-    }
-
-    public void setHorariosDisponibles(List<LocalTime> horariosDisponibles) {
-        this.horariosDisponibles = horariosDisponibles;
-    }
 }
